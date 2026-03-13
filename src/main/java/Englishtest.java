055fe3c008c11516264c990fba9e4e5060fc41e4
myung0322 <pmss0322@naver.com>
2026-03-13 15:16:50 +0900

    Initial commit

---
+++ src/main/java/Englishtest.java
@@ -0,0 +1,17 @@
+public class Englishtest {
+
+
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, my name is Myeong-je Jung.");
+
+        System.out.println("I am majoring in Big Data Software.");
+
+        System.out.println("My greatest strength is my persistence; I'm really good at hanging in there until the task is done");
+
+        System.out.println("My goal is to become a professional software developer");
+
+        System.out.println("Thank you for listening. Feel free to contact me anytime.");
+    }
+}
