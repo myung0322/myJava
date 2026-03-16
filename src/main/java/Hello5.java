abf10277005f09efd37f3bedb5d47a4e571d93fc
myung0322 <pmss0322@naver.com>
2026-03-16 09:50:22 +0900

    다섯번재 파일

--- src/main/java/Hello5.java
+++ src/main/java/Hello5.java
@@ -1,7 +1,10 @@
 public class Hello5 {
 
+
     public static void main(String[] args){
 
-        System.out.println("Hello My First Java Coding");
+        System.out.println("Hello!! My First Java Coding!!");
+
+        System.out.println("Edit Java Code!!");
     }
 }
