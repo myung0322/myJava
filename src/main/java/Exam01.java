04bcbe281fda56648640ad9920ee66530e7a2301
myung0322 <pmss0322@naver.com>
2026-03-23 10:46:08 +0900

    Scanner 입력값 응용

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exam01 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        num1 = s.nextInt();
+        num2 = s.nextInt();
+
+        double resDiv;
+        resDiv = (double) num1 / num2;
+
+        System.out.println("사용자가 입력한 값 ==> " + num1);
+        System.out.println("사용자가 입력한 값 ==> " + num2);
+        System.out.println(num1 + "/" + num2 + "=" + resDiv);
+
+    }
+}
