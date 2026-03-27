bd5e157f21b150050bd24f8d47b51a158786b576
myung0322 <pmss0322@naver.com>
2026-03-27 13:29:37 +0900

    곱하기,더하기 코딩

---
+++ src/main/java/Code03_5.java
@@ -0,0 +1,9 @@
+public class Code03_5 {
+    public static void main(String[] args){
+        int a = 3, b = 4, c = 5;
+
+        System.out.println(a * b + c);
+        System.out.println(c + a * b);
+        System.out.println((a + c) * b);
+    }
+}
